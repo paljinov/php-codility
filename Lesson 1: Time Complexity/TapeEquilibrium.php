a70b3cdcece22e6e77631db53a322c004a54513a
Pave Aljinović <paljinovic@gmail.com>
2014-12-12 14:05:51 +0100

    FrogJmp solution

--- Lesson 1: Time Complexity/TapeEquilibrium.php
+++ Lesson 1: Time Complexity/TapeEquilibrium.php
@@ -11,6 +11,7 @@ The difference between the two parts is the value of:
 
 In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
 For example, consider array A such that:
+
   A[0] = 3
   A[1] = 1
   A[2] = 2
@@ -18,17 +19,20 @@ For example, consider array A such that:
   A[4] = 3
 
 We can split this tape in four places:
+
         P = 1, difference = |3 − 10| = 7
         P = 2, difference = |4 − 9| = 5
         P = 3, difference = |6 − 7| = 1
         P = 4, difference = |10 − 3| = 7
 
 Write a function:
+
     function solution($A); 
 
 that, given a non-empty zero-indexed array A of N integers, returns the minimal difference that can be achieved.
 
 For example, given:
+
   A[0] = 3
   A[1] = 1
   A[2] = 2
