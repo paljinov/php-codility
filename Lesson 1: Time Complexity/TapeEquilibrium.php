d822ec04c98fae4b26208230bf353b9145a103f3
Pave Aljinović <paljinovic@gmail.com>
2014-12-12 13:09:10 +0100

    TapeEquilibrium solution

--- Lesson 1: Time Complexity/TapeEquilibrium.php
+++ Lesson 1: Time Complexity/TapeEquilibrium.php
@@ -49,3 +49,26 @@ Complexity:
 Elements of input arrays can be modified.
 */
 
+function solution($A) 
+{
+	// left sum after array $A splits, initialized to 0
+	$leftSum = 0;
+	// right sum after array $A splits, initialized to sum of all array $A elements
+	$rightSum = array_sum($A);
+	// minimal difference between left and right sum
+	$diff = null;
+
+	// at least one element needs to be on right side,
+	// that is the reason why we iterate to "count($A) - 1"
+	for($i = 0; $i < count($A) - 1; $i++) 
+	{
+		$leftSum += $A[$i];
+		$rightSum -= $A[$i];
+
+		// if diff is uninitialized, or difference is new absolute minimum difference
+		if($diff === null || abs($leftSum - $rightSum) < $diff)
+			$diff = abs($leftSum - $rightSum);
+	}
+
+	return $diff;
+}
