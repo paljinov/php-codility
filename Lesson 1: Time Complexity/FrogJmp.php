1e2506ebadaab17eba078bcde9fee66e753f565e
Pave Aljinović <paljinovic@gmail.com>
2014-12-12 14:06:51 +0100

    Typo

--- Lesson 1: Time Complexity/FrogJmp.php
+++ Lesson 1: Time Complexity/FrogJmp.php
@@ -37,7 +37,7 @@ Complexity:
 
 function solution($X, $Y, $D)
 {
-	// distance between end ($Y) and start (X) position
+	// distance between end ($Y) and start ($X) position
 	$distance = $Y - $X;
 	// number of jumps needed to reach or surpass end position
 	$jumps = (int)ceil($distance / $D);
