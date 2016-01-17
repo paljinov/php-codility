3cfe7932c4a3654a03904e2b26fa4fbaddeed7a6
Pave Aljinović <paljinovic@gmail.com>
2016-01-17 13:17:21 +0100

    Coding style is adjusted

--- Lesson 09 - Sieve of Eratosthenes/CountNonDivisible.php
+++ Lesson 09 - Sieve of Eratosthenes/CountNonDivisible.php
@@ -1,13 +1,13 @@
 <?php
 
-/* 
+/*
 You are given a non-empty zero-indexed array A consisting of N integers.
 
 For each number A[i] such that 0 ≤ i < N, we want to count the number of elements
 of the array that are not the divisors of A[i]. We say that these elements are non-divisors.
 
 For example, consider integer N = 5 and array A such that:
- 
+
     A[0] = 3
     A[1] = 1
     A[2] = 2
@@ -15,7 +15,7 @@ For example, consider integer N = 5 and array A such that:
     A[4] = 6
 
 For the following elements:
-  
+
     A[0] = 3, the non-divisors are: 2, 6,
     A[1] = 1, the non-divisors are: 3, 2, 3, 6,
     A[2] = 2, the non-divisors are: 3, 3, 6,
@@ -51,46 +51,50 @@ Assume that:
 
 Complexity:
     expected worst-case time complexity is O(N*log(N));
-    expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+    expected worst-case space complexity is O(N),
+    beyond input storage (not counting the storage required for input arguments).
 
 Elements of input arrays can be modified.
- */
+*/
 
 /*
- * CODILITY ANALYSIS: https://codility.com/demo/results/trainingDHSE37-F8M/
+ * CODILITY ANALYSIS: https://codility.com/demo/results/trainingXDX8PF-8NP/
  * LEVEL: MEDIUM
  * Correctness:	100%
  * Performance:	100%
  * Task score:	100%
  */
-function solution($A) 
+function solution($A)
 {
-    $N          = count($A);
-    
-    //calculate how many times number occurs in original array
-    $occurrence = array_fill(0, $N, 0);
-    foreach ($A as $val) {
-        $occurrence[$val] = (isset($occurrence[$val])) ? $occurrence[$val] + 1 : 1;        
-    }
-    
-    $nonDivisibles = [];
-    for ($i=0; $i < $N; $i++) {
-        //define divisors number
-        $divisorsCount = 0;
-        for($j=1; $j*$j <= $A[$i]; $j++) {
-            //if $j is divisor of $A[$i] add number of occurences $j to $divisorsCount
-            if ($A[$i] % $j == 0) {
-                $divisorsCount += $occurrence[$j];
-                
-                //also add remainder of the division as a $A[$i] divisor
-                if ($A[$i] / $j != $j) {
-                    $divisorsCount += $occurrence[$A[$i] / $j];
-                }
-            }
-        }
-        
-        //subtract divisors number from array length to get nonDivisible for element $i
-        $nonDivisibles[$i] = $N - $divisorsCount;
-    }
-    return $nonDivisibles;
+	$N = count($A);
+	// amount of non-divisors
+	$nonDivisibles = array();
+
+	// calculate how many times number occurs in original array
+	$occurrence = array_fill(0, $N, 0);
+	foreach($A as $val)
+		$occurrence[$val] = isset($occurrence[$val]) ? $occurrence[$val] + 1 : 1;
+
+	for($i = 0; $i < $N; $i++)
+	{
+		// define divisors number
+		$divisorsCount = 0;
+		for($j = 1; $j * $j <= $A[$i]; $j++)
+		{
+			// if $j is divisor of $A[$i] add number of occurences $j to $divisorsCount
+			if($A[$i] % $j == 0) 
+			{
+				$divisorsCount += $occurrence[$j];
+
+				// also add remainder of the division as a $A[$i] divisor
+				if($A[$i] / $j != $j)
+					$divisorsCount += $occurrence[$A[$i] / $j];
+			}
+		}
+
+		// subtract divisors number from array length to get nonDivisible for element $i
+		$nonDivisibles[$i] = $N - $divisorsCount;
+	}
+
+	return $nonDivisibles;
 }
