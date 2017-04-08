e4cbe02786dc7af799fed5b52c0d50ba841f7ec8
Pave Aljinović <paljinovic@gmail.com>
2017-04-08 20:51:34 +0200

    Code optimization and PSR-2 coding standard fix

--- Lesson 03 - Time Complexity/PermMissingElem.php
+++ Lesson 03 - Time Complexity/PermMissingElem.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
-A zero-indexed array A consisting of N different integers is given. 
+A zero-indexed array A consisting of N different integers is given.
 The array contains integers in the range [1..(N + 1)], which means that exactly one element is missing.
 
 Your goal is to find that missing element.
 
 Write a function:
 
-    function solution($A); 
+    function solution($A);
 
 that, given a zero-indexed array A, returns the value of the missing element.
 
@@ -28,47 +28,81 @@ Assume that:
 
 Complexity:
         expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(1), 
+        expected worst-case space complexity is O(1),
         beyond input storage (not counting the storage required for input arguments).
 
 Elements of input arrays can be modified.
 */
 
-/*
- * CODILITY ANALYSIS: https://codility.com/demo/results/demoDBGQT9-8VH/
+/**
+ * PermMissingElem task.
+ *
+ * CODILITY ANALYSIS: https://codility.com/demo/results/trainingV7P9M9-VKJ/
  * LEVEL: EASY
  * Correctness:	100%
  * Performance:	100%
  * Task score:	100%
+ *
+ * @param array $A Zero-indexed array A
+ *
+ * @return int Value of the missing element
  */
-function solution($A) 
+function solution($A)
 {
-	// this task can be easily solved by using Gauss sum formula for positive consecutive integers,
-	// also known as PERMUATION number array
-	// sum = (N * (N + 1)) / 2, where N is count of positive consecutive integers
-	// for example, sum of 1 + 2 + 3 + 4 + 5 = (5 * (5 + 1)) / 2 = (5 * 6) / 2 = 30 / 2 = 15
-	// according to legend, Gauss figured this out at the age of 8 during class in elementary school
-	//
-	// we know all integers are positive and different, so when we add missing element we 
-	// have Gauss formula
-	// 
-	// integer has 32-bit or 64 bit, depending on PHP build and platform, and 32 bit is limitation 
-	// for N * ($N + 1) when N = 100 000
-	// 32-bit int max number: 2147483647
-	// when N = 100 000, sum = 10000100000
-	// so we must use float
-	// http://php.net/manual/en/language.types.float.php
-	// "The size of a float is platform-dependent, although a maximum of ~1.8e308 with a precision 
-	// of roughly 14 decimal digits is a common value (the 64 bit IEEE format)."
-	
-	// number of positive integers including missing one
-	$N = count($A) + 1;
-	$sumIncludingMissingNumber = ((float)$N * ($N + 1)) / 2;
-	$sumWithoutMissingNumber = (float)0;
-	foreach($A as $number) 
-		$sumWithoutMissingNumber += $number;
+    // Missing element can be found by finding difference between
+    // sum of elements with missing element and sum of elements without missing element
 
-	$missingNumber = (int)($sumIncludingMissingNumber - $sumWithoutMissingNumber);
+    // Integer has 32-bit or 64 bit, depending on PHP build and platform, and 32 bit is limitation
+    // for N * ($N + 1) when N = 100 000
+    // 32-bit int max number: 2147483647
+    // when N = 100 000, sum = 10000100000
+    // so we must use float
+    // http://php.net/manual/en/language.types.float.php
+    // "The size of a float is platform-dependent, although a maximum of ~1.8e308 with a precision
+    // of roughly 14 decimal digits is a common value (the 64 bit IEEE format)."
 
-	return $missingNumber;
+    $sumIncludingMissingInteger = calculateSumIncludingMissingInteger($A);
+    $sumWithoutMissingInteger = calculateSumWithoutMissingInteger($A);
+
+    $missingInteger = (int) ($sumIncludingMissingInteger - $sumWithoutMissingInteger);
+
+    return $missingInteger;
 }
+
+/**
+ * Calculates sum of array A elements with addition of the missing one also.
+ *
+ * Gauss sum formula for positive consecutive integers, also known as PERMUATION number array is used:
+ * sum = (N * (N + 1)) / 2, where N is count of positive consecutive integers
+ * for example, sum of 1 + 2 + 3 + 4 + 5 = (5 * (5 + 1)) / 2 = (5 * 6) / 2 = 30 / 2 = 15.
+ * According to legend, Gauss figured this out at the age of 8 during class in elementary school.
+ *
+ * @param array $A
+ *
+ * @return float
+ */
+function calculateSumIncludingMissingInteger(array $A): float
+{
+    $N = count($A) + 1;
+    $sumIncludingMissingInteger = (float) ($N * ($N + 1)) / 2;
+
+    return $sumIncludingMissingInteger;
+}
+
+/**
+ * Calculates sum of array A elements.
+ *
+ * @param array $A
+ *
+ * @return float
+ */
+function calculateSumWithoutMissingInteger(array $A): float
+{
+    $sumWithoutMissingInteger = (float) 0;
+    foreach ($A as $integer) {
+        $sumWithoutMissingInteger += $integer;
+    }
+
+    return $sumWithoutMissingInteger;
+}
+
