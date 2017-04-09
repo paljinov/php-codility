fa21974ea30eaaefbd7b42a7e8b8cafd59174eea
Pave Aljinović <paljinovic@gmail.com>
2017-04-09 11:59:27 +0200

    Code optimization and PSR-2 coding standard fix

--- Lesson 04 - Counting Elements/MissingInteger.php
+++ Lesson 04 - Counting Elements/MissingInteger.php
@@ -3,18 +3,18 @@
 /*
 Write a function:
 
-    function solution($A); 
+    function solution($A);
 
-that, given a non-empty zero-indexed array A of N integers, 
+that, given a non-empty zero-indexed array A of N integers,
 returns the minimal positive integer that does not occur in A.
 
 For example, given:
 
-  A[0] = 1    
-  A[1] = 3    
+  A[0] = 1
+  A[1] = 3
   A[2] = 6
-  A[3] = 4    
-  A[4] = 1    
+  A[3] = 4
+  A[4] = 1
   A[5] = 2
 
 the function should return 5.
@@ -25,45 +25,59 @@ Assume that:
 
 Complexity:
         expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(N), 
+        expected worst-case space complexity is O(N),
         beyond input storage (not counting the storage required for input arguments).
 
 Elements of input arrays can be modified.
 */
 
-/*
- * CODILITY ANALYSIS: https://codility.com/demo/results/demo59YX59-S3E/
- * LEVEL: MEDIUM
- * Correctness:	100%
- * Performance:	100%
- * Task score:	100%
+/**
+ * MissingInteger task.
+ *
+ * CODILITY ANALYSIS: https://codility.com/demo/results/trainingREWHQK-9X7/
+ * LEVEL: EASY
+ * Correctness: 100%
+ * Performance: 100%
+ * Task score:  100%
+ *
+ * @param array $A Non-empty zero-indexed array A
+ *
+ * @return int Minimal positive integer that does not occur in A
  */
 function solution($A)
 {
-	// positive unique integers 
-	$positiveUnique = array();
-	for($i = 0; $i < count($A); $i++)
-		// if integer is positive, and not already in array
-		if($A[$i] > 0 && empty($positiveUnique[$A[$i]]))
-			// $positiveUnique array key matches value
-			$positiveUnique[$A[$i]] = $A[$i];
+    // Positive unique integers
+    $positiveUnique = [];
+    foreach ($A as $integer) {
+        // If integer is positive, and not already in array
+        if ($integer > 0 && empty($positiveUnique[$integer])) {
+            $positiveUnique[$integer] = $integer;
+        }
+    }
 
-	// if there is no positive integers, then the minimum positive integer not found is 1
-	if(count($positiveUnique) === 0)
-		return 1;
-	// if there are positive integers
-	else
-	{
-		// maximum positive unique integer
-		$max = max($positiveUnique);
-		// we are searching the first integer which is not in the $positiveUnique array
-		// upper iteration limit is maximum positive unique integer 
-		// because we are searching first missing one till the maximum
-		for($i = 1; $i < $max; $i++)
-			if(empty($positiveUnique[$i]))
-				return $i;	
-	}
+    if (count($positiveUnique) === 0) {
+        // If there are no positive integers, then the minimum positive integer not found is 1
+        $minPositiveNotOccurInA = 1;
+    } else {
+        // If there are positive integers
 
-	// if we have situation like this $A = [1, 2, 3, 4], first missing one is bigger then maximum
-	return $max + 1;
+        // Maximum positive unique integer
+        $maxPositiveInteger = max($positiveUnique);
+
+        // If sequence like this $A = [1, 2, 3, 4] is given, first missing positive integer is bigger
+        // than maximum positive unique integer, that is the reason why first missing positive integer
+        // is initialized to $maxPositiveInteger + 1
+        $minPositiveNotOccurInA = $maxPositiveInteger + 1;
+
+        // First integer which is not in the $positiveUnique array is searched for;
+        // upper limit is the maximum positive unique integer
+        for ($i = 1; $i < $maxPositiveInteger; $i++) {
+            if (!isset($positiveUnique[$i])) {
+                $minPositiveNotOccurInA = $i;
+                break;
+            }
+        }
+    }
+
+    return $minPositiveNotOccurInA;
 }
