c0ea64bc99ab2ef1575d7bc714fc761ba229ba40
Pave Aljinović <paljinovic@gmail.com>
2017-04-15 18:19:13 +0200

    Code optimization and PSR-2 coding standard fix

--- Lesson 05 - Prefix Sums/PassingCars.php
+++ Lesson 05 - Prefix Sums/PassingCars.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-A non-empty zero-indexed array A consisting of N integers is given. 
+A non-empty zero-indexed array A consisting of N integers is given.
 The consecutive elements of array A represent consecutive cars on a road.
 
 Array A contains only 0s and/or 1s:
@@ -9,7 +9,7 @@ Array A contains only 0s and/or 1s:
         0 represents a car traveling east,
         1 represents a car traveling west.
 
-The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N, 
+The goal is to count passing cars. We say that a pair of cars (P, Q), where 0 ≤ P < Q < N,
 is passing when P is traveling to the east and Q is traveling to the west.
 
 For example, consider array A such that:
@@ -24,7 +24,7 @@ We have five pairs of passing cars: (0, 1), (0, 3), (0, 4), (2, 3), (2, 4).
 
 Write a function:
 
-    function solution($A); 
+    function solution($A);
 
 that, given a non-empty zero-indexed array A of N integers, returns the number of passing cars.
 
@@ -46,37 +46,47 @@ Assume that:
 
 Complexity:
         expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(1), 
+        expected worst-case space complexity is O(1),
         beyond input storage (not counting the storage required for input arguments).
 
 Elements of input arrays can be modified.
 */
 
-/*
- * CODILITY ANALYSIS: https://codility.com/demo/results/demoQV2PE9-UDK/
+/**
+ * PassingCars task.
+ *
+ * CODILITY ANALYSIS: https://codility.com/demo/results/trainingB35TQC-JPE/
  * LEVEL: EASY
- * Correctness:	100%
- * Performance:	100%
- * Task score:	100%
+ * Correctness: 100%
+ * Performance: 100%
+ * Task score:  100%
+ *
+ * @param int[] $A non-empty zero-indexed array A
+ *
+ * @return int The number of passing cars
  */
 function solution($A)
 {
-	// passing cars counter
-	$passingCars = 0;
-	// counter for cars going east
-	$eastCounter = 0;
-	for($i = 0; $i < count($A); $i++)
-	{
-		// if car is going east
-		if($A[$i] === 0)
-			$eastCounter++;
-		// if car is going west, it passed over with every car which was going east till now
-		elseif($A[$i] === 1)
-			$passingCars += $eastCounter;
+    // Passing cars counter
+    $passingCars = 0;
+    // Counter for the cars going east
+    $eastCounter = 0;
 
-		if($passingCars > 1000000000)
-			return -1;		
-	}
+    foreach ($A as $carDirection) {
+        if ($carDirection === 0) {
+            // If the car is going east
+            $eastCounter++;
+        } else {
+            // If the car is going west, it passed over with every car which was going east till now
+            $passingCars += $eastCounter;
+        }
 
-	return $passingCars;
+        // The function should return −1 if the number of passing cars exceeds 1,000,000,000
+        if ($passingCars > 1000000000) {
+            $passingCars = -1;
+            break;
+        }
+    }
+
+    return $passingCars;
 }
