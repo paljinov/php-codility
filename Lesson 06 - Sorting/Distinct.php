d93f8fdfc35cb7dceee80a63dbdf32bd87037455
Pave Aljinović <paljinovic@gmail.com>
2017-04-16 18:23:03 +0200

    Code optimization and PSR-2 coding standard fix

--- Lesson 06 - Sorting/Distinct.php
+++ Lesson 06 - Sorting/Distinct.php
@@ -3,9 +3,9 @@
 /*
 Write a function
 
-    function solution($A); 
+    function solution($A);
 
-that, given a zero-indexed array A consisting of N integers, 
+that, given a zero-indexed array A consisting of N integers,
 returns the number of distinct values in array A.
 
 Assume that:
@@ -18,40 +18,47 @@ For example, given array A consisting of six elements such that:
     A[0] = 2    A[1] = 1    A[2] = 1
     A[3] = 2    A[4] = 3    A[5] = 1
 
-the function should return 3, because there are 3 distinct values appearing in array A, 
+the function should return 3, because there are 3 distinct values appearing in array A,
 namely 1, 2 and 3.
 
 Complexity:
         expected worst-case time complexity is O(N*log(N));
-        expected worst-case space complexity is O(N), 
+        expected worst-case space complexity is O(N),
         beyond input storage (not counting the storage required for input arguments).
 
 Elements of input arrays can be modified.
 */
 
-/*
- * CODILITY ANALYSIS: https://codility.com/demo/results/demoSA5K6M-E6Z/
+/**
+ * Distinct task.
+ *
+ * CODILITY ANALYSIS: https://codility.com/demo/results/trainingRXXFGB-DJM/
  * LEVEL: EASY
- * Correctness:	100%
- * Performance:	100%
- * Task score:	100%
+ * Correctness: 100%
+ * Performance: 100%
+ * Task score:  100%
+ *
+ * @param int[] $A Zero-indexed array A consisting of N integers
+ *
+ * @return int The number of distinct values in array A
  */
-function solution($A) 
+function solution($A)
 {
-	// this can be also solved with: return count(array_count_values($A));
-	// or: return count(array_flip(array_flip(array_reverse($A, true))));
+    // This can be also solved with: return count(array_count_values($A));
+    // or: return count(array_flip(array_flip(array_reverse($A, true))));
 
-	// array which contains distinct values of array $A
-	$distinct = array();
-	foreach($A as $value) 
-	{
-		// saving distinct values by key ensures uniqueness
-		if(empty($distinct[$value]))
-			$distinct[$value] = $value;
-	}
+    // Array which holds distinct values from array $A
+    $distinct = [];
 
-	// number of distinct values in array $A
-	$distinctCount = count($distinct);
+    foreach ($A as $integer) {
+        // Saving distinct values by key ensures uniqueness
+        if (!isset($distinct[$integer])) {
+            $distinct[$integer] = $integer;
+        }
+    }
 
-	return $distinctCount;
+    // The number of distinct values in array $A
+    $distinctCount = count($distinct);
+
+    return $distinctCount;
 }
