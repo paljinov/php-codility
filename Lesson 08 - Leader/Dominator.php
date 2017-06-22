b8c2f4657a03be0cd7c24256962b77340cc91c5a
Pave Aljinović <paljinovic@gmail.com>
2017-06-22 22:22:02 +0200

    Code optimization and PSR-2 coding standard fix

--- Lesson 08 - Leader/Dominator.php
+++ Lesson 08 - Leader/Dominator.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-A zero-indexed array A consisting of N integers is given. 
+A zero-indexed array A consisting of N integers is given.
 The dominator of array A is the value that occurs in more than half of the elements of A.
 
 For example, consider array A such that
@@ -10,15 +10,15 @@ For example, consider array A such that
     A[3] = 2    A[4] = 3    A[5] = -1
     A[6] = 3    A[7] = 3
 
-The dominator of A is 3 because it occurs in 5 out of 8 elements of A 
+The dominator of A is 3 because it occurs in 5 out of 8 elements of A
 (namely in those with indices 0, 2, 4, 6 and 7) and 5 is more than a half of 8.
 
 Write a function
 
-    function solution($A); 
+    function solution($A);
 
-that, given a zero-indexed array A consisting of N integers, 
-returns index of any element of array A in which the dominator of A occurs. 
+that, given a zero-indexed array A consisting of N integers,
+returns index of any element of array A in which the dominator of A occurs.
 The function should return −1 if array A does not have a dominator.
 
 Assume that:
@@ -35,48 +35,56 @@ the function may return 0, 2, 4, 6 or 7, as explained above.
 
 Complexity:
         expected worst-case time complexity is O(N);
-        expected worst-case space complexity is O(1), 
+        expected worst-case space complexity is O(1),
         beyond input storage (not counting the storage required for input arguments).
 
 Elements of input arrays can be modified.
 */
 
-/*
- * CODILITY ANALYSIS: https://codility.com/demo/results/demoDZNFKJ-R2U/
+/**
+ * Dominator task.
+ *
+ * CODILITY ANALYSIS: https://codility.com/demo/results/training5YQZWK-DX2/
  * LEVEL: EASY
- * Correctness:	100%
- * Performance:	100%
- * Task score:	100%
+ * Correctness: 100%
+ * Performance: 100%
+ * Task score:  100%
+ *
+ * @param array $A Zero-indexed array A consisting of N integers
+ *
+ * @return int The index of any element of array A in which the dominator of A occurs,
+ * or −1 if array A does not have a dominator
  */
 function solution($A)
 {
-	// integer count of occurrences
-	$integerOccurrences = array();
-	// the largest count of occurrences
-	$maxOccurrences = 0;
-	// key of integer which occurred most time
-	$maxOccurrencesKey = null;
+    // Number of occurrences of each integer
+    $integerOccurrences = [];
+    // Maximum number of occurrences
+    $maxOccurrences = 0;
+    // Index of integer with maximum occurrences
+    $maxOccurrencesKey = null;
 
-	foreach($A as $key => $value) 
-	{
-		if(empty($integerOccurrences[$value]))
-			$integerOccurrences[$value] = 1;
-		else
-			$integerOccurrences[$value]++;
+    foreach ($A as $key => $value) {
+        if (empty($integerOccurrences[$value])) {
+            $integerOccurrences[$value] = 1;
+        } else {
+            $integerOccurrences[$value]++;
+        }
 
-		// if maximum occurrence got larger
-		if($integerOccurrences[$value] > $maxOccurrences)
-		{
-			$maxOccurrences = $integerOccurrences[$value];
-			// storing key of maximum occured integer
-			$maxOccurrencesKey = $key;
-		}
-	}
+        // Searching for integer with maxiumum occurences, and index of that integer
+        if ($integerOccurrences[$value] > $maxOccurrences) {
+            $maxOccurrences = $integerOccurrences[$value];
+            $maxOccurrencesKey = $key;
+        }
+    }
 
-	// if key of max occured integer is not set, 
-	// or max occured integer doesn't occurs in more than half of the elements of $A
-	if($maxOccurrencesKey === null || ($maxOccurrences <= count($A) / 2))
-		return -1;
-	else
-		return $maxOccurrencesKey;
+    // Number of integers in array $A
+    $N = count($A);
+    // If index of integer with maxiumum occurences is not set,
+    // or integer with maxiumum occurences doesn't occur in more than half of the elements of $A
+    if ($maxOccurrencesKey === null || ($maxOccurrences <= $N / 2)) {
+        return -1;
+    }
+
+    return $maxOccurrencesKey;
 }
