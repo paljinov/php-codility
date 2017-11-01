9b7b7a0ceb589a4d8b5ca4be23f4e73b268d8b37
Pave Aljinović <paljinovic@gmail.com>
2017-11-01 12:51:06 +0100

    MinPerimeterRectangle code optimization and PSR-2 coding standard fix

--- Lesson 10 - Prime and composite numbers/MinPerimeterRectangle.php
+++ Lesson 10 - Prime and composite numbers/MinPerimeterRectangle.php
@@ -3,10 +3,10 @@
 /*
 An integer N is given, representing the area of some rectangle.
 
-The area of a rectangle whose sides are of length A and B is A * B, 
+The area of a rectangle whose sides are of length A and B is A * B,
 and the perimeter is 2 * (A + B).
 
-The goal is to find the minimal perimeter of any rectangle whose area equals N. 
+The goal is to find the minimal perimeter of any rectangle whose area equals N.
 The sides of this rectangle should be only integers.
 
 For example, given integer N = 30, rectangles of area 30 are:
@@ -18,9 +18,9 @@ For example, given integer N = 30, rectangles of area 30 are:
 
 Write a function:
 
-    function solution($N); 
+    function solution($N);
 
-that, given an integer N, 
+that, given an integer N,
 returns the minimal perimeter of any rectangle whose area is exactly equal to N.
 
 For example, given an integer N = 30, the function should return 22, as explained above.
@@ -33,46 +33,70 @@ Complexity:
         expected worst-case space complexity is O(1).
 */
 
-/*
- * CODILITY ANALYSIS: https://codility.com/demo/results/demoRE8TP2-TD7/
+/**
+ * MinPerimeterRectangle task.
+ *
+ * CODILITY ANALYSIS: https://codility.com/demo/results/trainingZJEQMD-TAC/
  * LEVEL: EASY
- * Correctness:	100%
- * Performance:	100%
- * Task score:	100%
+ * Correctness: 100%
+ * Performance: 100%
+ * Task score:  100%
+ *
+ * @param array $N Rectangle area
+ *
+ * @return int The minimal rectangle perimeter
  */
+
 function solution($N)
 {
-	// all combinations of rectangle sides variations
-	$rectangleSideCombinations = array();
+    // All combinations of rectangle sides variations
+    $rectangleSidesCombinations = getPossibleSides($N);
 
-	// for example, if N = 30, 
-	// A can be [1, 2, 3, 5], 
-	// resulting in B which can be [30, 15, 10, 6]
+    // Minimal rectangle perimeter
+    $minPerimeter = null;
+    foreach ($rectangleSidesCombinations as $rectangleSides) {
+        $perimeter = 2 * ($rectangleSides['A'] + $rectangleSides['B']);
 
-	// we use formula to find half of $N divisors, we don't need to find another half, 
-	// because rectangle sides would just be mirrored, 
-	// respectively we would have 2 exactly the same perimeters
-	$i = 1;
-	$j = 0;
-	while($i * $i <= $N)
-	{
-		if($N % $i == 0)
-		{	
-			$rectangleSideCombinations[$j]['A'] = $i;
-			$rectangleSideCombinations[$j]['B'] = $N / $i;
-			$j++;
-		}
-		$i++;
-	}
+        if ($minPerimeter === null || $perimeter < $minPerimeter) {
+            $minPerimeter = $perimeter;
+        }
+    }
 
-	// minimal rectangle perimeter
-	$minPerimeter = null;
-	foreach($rectangleSideCombinations as $rectangleSides)
-	{
-		$perimeter = 2 * ($rectangleSides['A'] + $rectangleSides['B']);
-		if($minPerimeter === null || $perimeter < $minPerimeter)
-			$minPerimeter = $perimeter;
-	}
+    return $minPerimeter;
+}
 
-	return $minPerimeter;
+/**
+ * Get possible rectangle sides for area $N.
+ *
+ * @param int $N Area $N
+ *
+ * @return array Side combinations
+ * [
+ *     [
+ *         'A' => int,
+ *         'B' => int
+ *     ],
+ *     ...
+ * ]
+ */
+function getPossibleSides(int $N): array
+{
+    $rectangleSidesCombinations = [];
+
+    // For example, if N = 30, A can be [1, 2, 3, 5], resulting in B which can be [30, 15, 10, 6];
+    // we don't need to find another half of $N divisors because rectangle sides would be just mirrored,
+    // so formula to find half of $N divisors is used,
+
+    $i = 1;
+    while ($i * $i <= $N) {
+        if ($N % $i == 0) {
+            $rectangleSidesCombinations[] = [
+                'A' => $i,
+                'B' => $N / $i
+            ];
+        }
+        $i++;
+    }
+
+    return $rectangleSidesCombinations;
 }
